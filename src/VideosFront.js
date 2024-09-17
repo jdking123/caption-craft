0c8a9266cf855049f6ffe4f056a41e735f8482c5
jeevith <jeevithdharanish98@example.com>
2024-09-18 00:56:58 +0530

    error

--- src/VideosFront.js
+++ src/VideosFront.js
@@ -1,7 +1,7 @@
 export default function VideosFront() {
   return (
     <div className="hidden border-2 broder-white sm:block bg-gray-800/50 w-[280px] h-[400px] rounded-xl mx-auto overflow-hidden">
-      <video autoPlay loop muted controls className="w-full h-full object-cover">
+      <video autoPlay loop muted  className="w-full h-full object-cover">
         {/* Directly reference the video file from the public directory */}
         <source src="/vids/withcaps.mp4" type="video/mp4" />
         Your browser does not support the video tag.
--- src/VideosFront2.js
+++ src/VideosFront2.js
@@ -4,7 +4,7 @@
  export default function VideosFront2() {
   return (
     <div className="hidden border-2 broder-white sm:block bg-gray-800/50 w-[240px] rounded-xl overflow-hidden">
-    <video autoPlay loop muted controls >
+    <video autoPlay loop muted  >
           {/* Directly reference the video file from the public directory */}
           <source src="/vids/withoutcaps.mp4" type="video/mp4" />
           Your browser does not support the video tag.
